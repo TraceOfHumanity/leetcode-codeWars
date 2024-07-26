785e97e6e3e4cf37e126b333e6935af443459a31
Alexander_Ovchar <azalexxx02@gmail.com>
2024-07-26 16:12:19 +0300

    refactor: Simplify createPhoneNumber function

--- codeWars/6kuy/CreatePhoneNumber.js
+++ codeWars/6kuy/CreatePhoneNumber.js
@@ -4,6 +4,17 @@ function createPhoneNumber(numbers){
   return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`;
 }
 
+function createPhoneNumber2(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
+
 console.log(createPhoneNumber([1,2,3,4,5,6,7,8,9,0])) // should return "(123) 456-7890"
 console.log(createPhoneNumber([1,1,1,1,1,1,1,1,1,1])) // should return "(111) 111-1111"
 console.log(createPhoneNumber([0,0,0,0,0,0,0,0,0,0])) // should return "(000) 000-0000"
