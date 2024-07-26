dfa77e3efd079b88032a478ccb7bbd8c19f72139
Alexander_Ovchar <azalexxx02@gmail.com>
2024-07-26 16:12:34 +0300

    feat: Add function to create phone number from an array of integers

--- codeWars/6kuy/CreatePhoneNumber.js
+++ codeWars/6kuy/CreatePhoneNumber.js
@@ -20,3 +20,9 @@ console.log(createPhoneNumber([1,1,1,1,1,1,1,1,1,1])) // should return "(111) 11
 console.log(createPhoneNumber([0,0,0,0,0,0,0,0,0,0])) // should return "(000) 000-0000"
 console.log(createPhoneNumber([9,8,7,6,5,4,3,2,1,0])) // should return "(987) 654-3210"
 console.log(createPhoneNumber([0,1,2,3,4,5,6,7,8,9])) // should return "(012) 345-6789"
+
+console.log(createPhoneNumber2([1,2,3,4,5,6,7,8,9,0])) // should return "(123) 456-7890"
+console.log(createPhoneNumber2([1,1,1,1,1,1,1,1,1,1])) // should return "(111) 111-1111"
+console.log(createPhoneNumber2([0,0,0,0,0,0,0,0,0,0])) // should return "(000) 000-0000"
+console.log(createPhoneNumber2([9,8,7,6,5,4,3,2,1,0])) // should return "(987) 654-3210"
+console.log(createPhoneNumber2([0,1,2,3,4,5,6,7,8,9])) // should return "(012) 345-6789"
