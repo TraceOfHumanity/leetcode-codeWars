f3832488d215873155544d73d392a36284fca3db
Alexander_Ovchar <azalexxx02@gmail.com>
2024-07-28 15:32:46 +0300

    getDivisorsCnt

---
+++ codeWars/7kuy/CountTheDivisorsOfANumber.js
@@ -0,0 +1,24 @@
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000, but fixed tests go higher.
+
+// Examples (input --> output)
+// 4 --> 3 // we have 3 divisors - 1, 2 and 4
+// 5 --> 2 // we have 2 divisors - 1 and 5
+// 12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+// 30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30  
+
+function getDivisorsCnt(n) {
+  let count = 0;
+  for (let i = 1; i * i <= n; i++) {
+    if (n % i === 0) {
+      count += (i * i === n) ? 1 : 2;
+    }
+  }
+  return count;
+}
+
+console.log(getDivisorsCnt(4)) // should return 3
+console.log(getDivisorsCnt(5)) // should return 2
+console.log(getDivisorsCnt(12)) // should return 6
+console.log(getDivisorsCnt(30)) // should return 8
