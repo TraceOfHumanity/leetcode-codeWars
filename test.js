ac1b666a02b974927a80de7e9e787e30a245720e
Alexander_Ovchar <99219332+TraceOfHumanity@users.noreply.github.com>
2024-08-11 11:59:25 +0300

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const a = (value) => {
-  return value.split("").map((char) => {
-    const binary = char.charCodeAt(0);
-    return binary.toString(2).padStart(8, "0");
-  })
-};
-
-console.log(a("hello"));
