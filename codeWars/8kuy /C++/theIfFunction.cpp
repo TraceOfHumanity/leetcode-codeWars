a12cb2a04110e8170e28625488fa981455514f82
Alexander_Ovchar <azalexxx02@gmail.com>
2024-10-02 10:20:45 +0300

    _if

---
+++ codeWars/8kuy /C++/theIfFunction.cpp
@@ -0,0 +1,21 @@
+// Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+
+// When bool is truthy, func1 should be called, otherwise call the func2.
+
+// Example:
+// void TheTrue() { std::cout << "true" }
+// void TheFalse() { std::cout << "false" }
+// _if(true, TheTrue, TheFalse);
+// // Logs 'true' to the console.
+
+void _if(bool value, std::function<void(void)> func1, std::function<void(void)> func2)
+{
+  if (value)
+  {
+    func1();
+  }
+  else
+  {
+    func2();
+  }
+}
