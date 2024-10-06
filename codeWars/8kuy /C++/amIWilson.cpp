ec4b83ae4f3d1d2f058c27699785af8b42d1618c
Alexander_Ovchar <azalexxx02@gmail.com>
2024-10-06 22:09:51 +0300

    amIWilson

---
+++ codeWars/8kuy /C++/amIWilson.cpp
@@ -0,0 +1,22 @@
+// Wilson primes satisfy the following condition. Let
+// P
+// P represent a prime number.
+
+// Then,
+
+// ( P - 1 )! + 1 / P * P ​
+
+// should give a whole number, where
+// P
+// !
+// P! is the factorial of
+// P
+// P.
+
+// Your task is to create a function that returns true if the given number is a Wilson prime and false otherwise.
+
+bool amIWilson(unsigned n) {
+  if (n <= 1) return false;
+  
+  return false;
+}
