154adaa93f470e3872a875bd900d8278fb6a0da5
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-21 22:04:07 +0200

    factorial

--- codeWars/7kuy/TS/factorial.ts
+++ codeWars/7kuy/TS/factorial.ts
@@ -1,7 +1,8 @@
 // Your task is to write function factorial.
 
-export function factorial(n: number) {
+export function factorial(n: number): number {
   if (n === 0) return 1;
+  if (n === 1) return 1;
   return n * factorial(n - 1);
 }
 
