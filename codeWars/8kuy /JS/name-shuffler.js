71f7c7202814c1f8f634fe12f575d398aeb49568
TraceOfHumanity <azalexxx02@gmail.com>
2026-08-31 23:23:02 +0300

    Add name shuffler function to reverse the order of names in a string

---
+++ codeWars/8kuy /JS/name-shuffler.js
@@ -0,0 +1,7 @@
+function nameShuffler(str) {
+  return str.split(" ").reverse().join(" ");
+}
+
+console.log(nameShuffler("john doe"));
+console.log(nameShuffler("jane doe"));
+console.log(nameShuffler("john doe"));
