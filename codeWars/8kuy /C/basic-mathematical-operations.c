58a8f80b57b7d17e6fc57d1b5d8e7463bf059f39
TraceOfHumanity <azalexxx02@gmail.com>
2026-09-01 00:01:36 +0300

    Refactor mathematical operations to improve code structure and readability, ensuring consistent handling of edge cases.

---
+++ codeWars/8kuy /C/basic-mathematical-operations.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+double basicOp(char operation, double value1, double value2);
+
+int main() {
+  printf("%.0f\n", basicOp('+', 4, 7));   // 11
+  printf("%.0f\n", basicOp('-', 15, 18)); // -3
+  printf("%.0f\n", basicOp('*', 5, 5));   // 25
+  printf("%.0f\n", basicOp('/', 49, 7));  // 7
+}
+double basicOp(char operation, double value1, double value2) {
+  switch (operation) {
+  case '+':
+    return value1 + value2;
+  case '-':
+    return value1 - value2;
+  case '*':
+    return value1 * value2;
+  case '/':
+    return value1 / value2;
+  }
+}
